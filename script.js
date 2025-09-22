063ff009576178b4d1567d5a09ff3f900f8bce9b
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-09-22 19:22:20 +0530

    added html and css

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("started")
+console.log("started");
+
+let plus = document.querySelector(".plus"); // target correct button
+let list = document.querySelector(".list");
+let box = document.querySelector(".box");
+
+plus.addEventListener("click", () => {
+  let newBox = box.cloneNode(true); 
+  list.appendChild(newBox);
+});
