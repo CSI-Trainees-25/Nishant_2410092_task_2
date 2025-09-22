dda76be0c51eac4e3dac0bc6de987f9dae2b6419
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-09-22 22:12:59 +0530

    added some js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 console.log("started");
 
-let plus = document.querySelector(".plus"); // target correct button
+let plus = document.querySelector(".plus"); 
 let list = document.querySelector(".list");
 let box = document.querySelector(".box");
+let popup = document.querySelector(".popcont"); 
+let btn = document.querySelector(".btn button"); // <-- target the button only
 
 plus.addEventListener("click", () => {
-  let newBox = box.cloneNode(true); 
-  list.appendChild(newBox);
+   popup.style.display = "flex"; 
+});
+
+btn.addEventListener("click", () => {
+    let newBox = document.createElement("div");
+    newBox.classList.add("box");    
+    newBox.innerHTML = box.innerHTML;  
+    list.appendChild(newBox);
+    popup.style.display = "none";    
 });
