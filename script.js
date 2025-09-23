84f6bdc32c62826d4fafd991908c0005dcd2d0bc
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-09-23 10:04:19 +0530

    added js list

--- script.js
+++ script.js
@@ -4,16 +4,62 @@ let plus = document.querySelector(".plus");
 let list = document.querySelector(".list");
 let box = document.querySelector(".box");
 let popup = document.querySelector(".popcont"); 
-let btn = document.querySelector(".btn button"); // <-- target the button only
+let btn = document.querySelector(".btn button"); 
+
+let nameinput = document.querySelector(".name");
+let selectpr1 = document.querySelector("#pr1");
+let selectpr2 = document.querySelector("#pr2");
+let dateinput = document.querySelector(".date input");
 
 plus.addEventListener("click", () => {
    popup.style.display = "flex"; 
 });
 
 btn.addEventListener("click", () => {
-    let newBox = document.createElement("div");
-    newBox.classList.add("box");    
-    newBox.innerHTML = box.innerHTML;  
-    list.appendChild(newBox);
     popup.style.display = "none";    
+});
+
+btn.addEventListener("click", () => {
+    let name = nameinput.value;
+    console.log(name);
+        if (!name) {
+        alert("Enter task please !!!");
+        return;
+    }
+        let pr1input = selectpr1.value;
+    let pr2input = selectpr2.value;
+    let dateinp = dateinput.value;
+
+
+       let item = document.createElement("div");
+    item.className = "taskitem";
+
+       let pr1Options = selectpr1.innerHTML;
+    let pr2Options = selectpr2.innerHTML;
+
+
+            item.innerHTML = `
+        <div class="t"><strong>Task:</strong> ${name}</div>
+        <div class="bflex">
+            <div>
+                <label>Progress:</label>
+                <select>${pr1Options}</select>
+            </div>
+            <div>
+                <label>Level:</label>
+                <select>${pr2Options}</select>
+            </div>
+        </div>
+        <div class="due">
+            <label>Due Date:</label>
+            <input type="date" value="${dateinp}">
+        </div>
+    `;
+      
+        list.appendChild(item);
+        
+  nameinput.value = "";
+    dateinput.value = "";
+         selectpr1.selectedIndex = 0;
+    selectpr2.selectedIndex = 0;
 });
